fde44e6871c7eb7c0bf2d91969e5d5d015860d90
GustavoGiglio <gtsgiglio@gmail.com>
2023-05-08 21:23:27 -0300

    create project

---
+++ list/src/main/java/br/edu/ifrs/restinga/list/ListApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.restinga.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListApplication.class, args);
+	}
+
+}
